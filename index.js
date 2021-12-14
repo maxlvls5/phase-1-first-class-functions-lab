6d827060c6064184c0c6201c6519fcec1bf3e5f3
Shane <MalithSM@hotmail.com>
2021-12-14 17:49:14 -0600

    commit tree 22d140e6e889f262a23a458b320a79dc56e9df6c

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+//let pokemon =["pikachu", "charmander", "squirtle"]
+//const drivers = ['Antonia', 'Nuru', 'Amari', ''
+
+const returnFirstTwoDrivers = (drivers) => {
+  let arrayDrivers = [];
+  arrayDrivers.push(drivers[0], drivers[1]);
+  return arrayDrivers;
+};
+
+//console.log(returnFirstTwoDrivers(pokemon))
+
+const returnLastTwoDrivers = (drivers) => {
+  let arrayDrivers = [];
+  arrayDrivers.push(drivers[drivers.length - 2], drivers[drivers.length - 1]);
+  return arrayDrivers;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+  const fareMultiplier = (fare) => {
+    return fare * number;
+  };
+  return fareMultiplier;
+}
+const fareDoubler = (fare) => {
+  return createFareMultiplier(2)(fare);
+};
+const fareTripler = (fare) => {
+  return createFareMultiplier(3)(fare);
+};
+function selectDifferentDrivers(drivers, returnDrivers) {
+  return returnDrivers(drivers);
+}
